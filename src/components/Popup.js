0a2e319cd22d3ef735322a052fa31c3b7b3f1199
Shubham Agrawal <shubham.agrawal1@sprinkl.com>
2022-07-06 17:20:14 +0530

    moved headings to center

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,7 +3,7 @@ import "./../style.css";
 export function Popup(marker) {
   return `
     <div class="box-2">
-          <h2>Dropdown</h2>
+          <h2>Drilldown</h2>
           <hr></hr>
           <p class="popupPara">${marker.name}</p>
         </div>`;
